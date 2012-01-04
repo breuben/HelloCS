d7e4871f379836badf6200a5d861c18c16e63b02
Ben Reuben <breuben@gmail.com>
2012-01-03 16:52:18 -0800

    Made the program more interactive and personal.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace Hello
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello, world!");
+			Console.WriteLine();
+			Console.Write("What is your name? ");
+			string name = Console.ReadLine();
+			Console.WriteLine("Hello, {0}!", name);
 		}
 	}
 }
